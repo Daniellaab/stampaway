645f3bb4bad0223c491bddf54a9d9fff3b4500e1
Daniellaab <dab.denmark@gmail.com>
2023-12-05 21:27:55 +0100

    Navigation på plads (login+registration)

--- components/user/LoginScreen.js
+++ components/user/LoginScreen.js
@@ -1,17 +1,19 @@
-// In LoginScreen.js
 import React, { useState } from 'react';
 import { View, TextInput, Button, Alert } from 'react-native';
 import { useAuth } from '../../AuthenticationService'; // Make sure to use the correct path
+import { useNavigation } from '@react-navigation/native'; // Import useNavigation hook
 
 const LoginScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const { signInUser } = useAuth(); // Use the correct function name
+  const { signInUser } = useAuth();
+  const navigation = useNavigation(); // Get navigation object from the hook
 
   const handleLogin = async () => {
     try {
-      await signInUser(email, password); // Use the correct function name
+      await signInUser(email, password);
       Alert.alert('Success', 'User logged in successfully!');
+      navigation.navigate('NextScreen'); // Navigate to NextScreen
     } catch (error) {
       Alert.alert('Error', error.message);
     }
--- components/user/RegistrationScreen.js
+++ components/user/RegistrationScreen.js
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
 import { View, TextInput, Button, Alert } from 'react-native';
 import { useAuth } from '../../AuthenticationService'; 
+import { useNavigation } from '@react-navigation/native'; // Import useNavigation hook
 
 const RegistrationScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const { registerUser } = useAuth(); // Use the correct function name
+  const navigation = useNavigation(); // Get navigation object from the hook
 
   const handleRegister = async () => {
     try {
       await registerUser(email, password); // Use the correct function name
       Alert.alert('Success', 'User registered successfully!');
+      navigation.navigate('NextScreen'); // Navigate to NextScreen
     } catch (error) {
       Alert.alert('Error', error.message);
     }
