f6aace8ba10476ce7bc44467a62c05f658ac6d3b
Daniellaab <dab.denmark@gmail.com>
2023-12-05 15:13:38 +0100

    Opsætning af database

--- App.js
+++ App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { getApps, initializeApp } from "firebase/app";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+// Hovedfunktionen, der definerer den samlede app-komponent
+function App() {
+// Firebase konfigurationsobjekt, der indeholder API-nøgler og andre opsætningsoplysninger
+  const firebaseConfig = {
+    apiKey: "AIzaSyANhYOvCJRE8Ue8cdf6Ib3Z5EGQLvElUVw",
+    authDomain: "stampaway-64e61.firebaseapp.com",
+    databaseURL: "https://stampaway-64e61-default-rtdb.europe-west1.firebasedatabase.app/",
+    projectId: "stampaway-64e61",
+    storageBucket: "stampaway-64e61.appspot.com",
+    messagingSenderId: "1011228104584",
+    appId: "1:1011228104584:web:bbe3892fdbf3b59a324c3d"
+  };
+  if (getApps().length < 1) {
+    initializeApp(firebaseConfig);
+    console.log('Firebase On!'); // Udskriver besked i konsollen, når Firebase er aktiveret
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// Eksporterer App-komponenten som standard eksport for at gøre den tilgængelig for resten af applikationen
+export default App;
